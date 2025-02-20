84e6631c9fd8923b2bcd9b7755851ccad03e9753
aquib116 <rockygaming85400@gmail.com>
2025-02-20 20:50:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,7 +18,7 @@ async def start(client, message):
     user = message.from_user
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id)             
-    txt=f"👋 Hello Developer {user.mention} \n\nI am an Advance file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document !"
+    txt=f"👋 Hello Developer {user.mention} \n\nI am an Advance 2gb file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document !"
     button=InlineKeyboardMarkup([[
         InlineKeyboardButton(" Developer ", url='https://t.me/ExE_AQUIB')
         ],[
